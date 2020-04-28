69ae741f53cc237a5283ba479445ca6ca7bbf4ec
Samvel <41449047+sttie@users.noreply.github.com>
2020-04-28 15:14:49 +0300

    Update figures.py

--- figures.py
+++ figures.py
@@ -1,24 +1,40 @@
+class Graph:
+    def __init__(self, vertex_amount):
+        self.vertex_amount = vertex_amount
+        self.matrix = []
+        for i in range(vertex_amount):
+            self.matrix.append([0] * vertex_amount)
+
+    # Добавление ребра edge = (vertex1, vertex2) с весом weight
+    def add_edge(self, edge, weight):
+        vertex1, vertex2 = edge
+
+        self.matrix[vertex1][vertex2] = weight
+        self.matrix[vertex2][vertex1] = weight
+
+    def get_weight(self, vertex1, vertex2):
+        return self.matrix[vertex1][vertex2]
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-
     def __add__(self, other):
-        self.x += other.x
-        self.y += other.y
-    
+        return Point(self.x + other.x, self.y + other.y)
     
     def __sub__(self, other):
-        self.x -= other.x
-        self.y -= other.y
+        return Point(self.x - other.x, self.y - other.y)
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
-
     
     def __str__(self):
         return "({}, {})".format(self.x, self.y)
+
+    def __hash__(self):
+        return self.x.__hash__() ^ self.y.__hash__()
 
 
 class Segment:
@@ -27,14 +43,22 @@ class Segment:
         self.start = start
         self.end = end
     
+    def __eq__(self, another):
+        return self.start == another.start and self.end == another.end or self.end == another.start and self.start == another.end
+
+    def __getitem__(self, key):
+        if not key in [0, 1]:
+            raise IndexError
+        
+        return self.start if key == 0 else self.end
+
     def __str__(self):
-        return "({}, {})".format(self.start, self.end)
+        return "[{}, {}]".format(self.start, self.end)
 
 
 class Polygon:
     def __init__(self, points=[]):
         self.points = points
-
 
     def add_point_xy(self, x, y):
         self.points.append(Point(x, y))
@@ -42,32 +66,18 @@ class Polygon:
     def add_point_p(self, point):
         self.points.append(point)
 
+    def index(self, obj):
+        return self.points.index(obj)
 
     def inp(self, point):
         return point in self.points
 
-
     def __getitem__(self, key):
         return self.points[key]
 
-    
     def __len__(self):
         return len(self.points)
     
-
     def __iter__(self):
-        self.it = 0
-        return self   
-
-
-    def __next__(self):
-        if self.it >= len(self.points):
-            raise StopIteration   
-         
-        self.it += 1
-        return self.points[self.it - 1]
-
-    
-    def __str__(self):
-        for p in self.points:
-            yield p
+        for point in self.points:
+            yield point
