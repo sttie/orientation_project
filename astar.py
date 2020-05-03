089e7537930bf5a87cd534f98c0ad28808b6cf22
Samvel <41449047+sttie@users.noreply.github.com>
2020-05-03 17:18:41 +0300

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,6 +1,4 @@
-# Ширина окна - 1000, высота - 700 => максимальная (округленная) длина ребра - 1221
-MAXLEN = 1221
-
+# Очередь с приоритетом
 class PriorityQueue:
     def __init__(self):
         self.queue = []
@@ -25,21 +23,9 @@ class PriorityQueue:
     def is_empty(self):
         return self.size == 0
 
-    def is_in(self, element):
-        for v in self.queue:
-            if v[0] == element:
-                return 1
-        
-        return 0
-    
-    def pop_value(self, value):
-        for i in range(self.size):
-            if self.queue[i][0] == value:
-                self.queue.pop(i)
-
 
 """
-Эвристическая оценка h(v) - евклидово расстояние от текущей точки до конечной
+Эвристическая оценка h(v) - евклидово расстояние от точки v до конечной
 """
 def heuristics(v, end):
     return ((v.x - end.x)**2 + (v.y - end.y)**2)**(1/2)
@@ -48,8 +34,7 @@ def heuristics(v, end):
 В процессе работы рассчитывается функция f(v) = g(v) + h(v), где
 g(v) - наименьшая стоимость пути в v из start_point
 h(v) - эвристическое приближение стоимости пути от v до end_point
-Открытые алгоритмом вершины можно хранить в очереди с приоритетом по значению f(v)
-# visited - Q, lengths_from_start - g, target_func - f
+Открытые алгоритмом вершины хранятся в очереди с приоритетом по значению f(v)
 """
 def astar_algo(graph, start, end, all_points):
     to_visit = PriorityQueue()
@@ -75,47 +60,6 @@ def astar_algo(graph, start, end, all_points):
                 ancestors[v] = current_vert
 
     return find_path(start, end, ancestors, all_points)
-
-
-# def astar_algo(graph, start, end, all_points):
-#     to_visit = PriorityQueue()
-#     to_visit.push(start, 0)
-#     visited = [0] * graph.vertex_amount
-#     lengths_from_start = [0] * graph.vertex_amount
-#     ancestors = [0] * graph.vertex_amount
-
-#     to_visit.push(start, 0)
-#     lengths_from_start[start] = 0
-
-#     while not to_visit.is_empty():
-#         current_vert = to_visit.pop_front()[0]
-#         # нужно ли оставить? поменяется ли кратчайший путь?
-#         if current_vert == end:
-#             break
-        
-#         visited[current_vert] = 1
-
-#         # range(graph.vertex_amount) можно заменить graph.get_neighbours(current_vert)
-#         for v in range(graph.vertex_amount):
-#             # нет смысла проверять не смежные с current_vert вершины
-#             if graph.matrix[current_vert][v] == 0:
-#                 continue
-
-#             cost = lengths_from_start[current_vert] + graph.get_weight(current_vert, v)
-
-#             # ВСЕ МЕНЯЕТ NOT VISITED[V] - ДОЛЖНО БЫТЬ NOT lengths_from_start[v]
-
-#             # Если v уже была посещена и текущая оценка >=, то пропускаем ее
-#             if visited[v] and cost >= lengths_from_start[v]:
-#                 continue
-#             if not visited[v] or cost < lengths_from_start[v]:
-#                 ancestors[v] = current_vert
-#                 lengths_from_start[v] = cost
-#                 priority = lengths_from_start[v] + heuristics(all_points[v], all_points[end])
-#                 if not to_visit.is_in(v):
-#                     to_visit.push(v, priority)
-
-#     return find_path(start, end, ancestors, all_points)
 
 
 def find_path(start, end, ancestors, all_points):
