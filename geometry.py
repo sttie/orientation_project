b3747fa4f2068650f63db950e5b913c109761c93
Samvel <41449047+sttie@users.noreply.github.com>
2020-05-05 16:32:04 +0300

    Update geometry.py

--- geometry.py
+++ geometry.py
@@ -1,20 +1,57 @@
-from intersections import check_point_in_polygon
 from figures import Point
 
-# Добавляем 4 доп. вершины к каждой из вершин полигона
+
+# Сложно в комментариях объяснить все эти формулы
+# Функция берет две точки и строит прямоугольник с одной из сторон, равной radius,
+# с этими точками по центру двух противоположных сторон
+def get_rectangle(point, point2, radius):
+    x1, x2, y1, y2 = point.x, point2.x, point.y, point2.y
+    
+    # Если координаты по иксу равны, то с точностью можно сказать об игриках
+    if x1 == x2:
+        x3_1 = radius + x2
+        x3_2 = -radius + x2
+        x4_1 = radius + x1
+        x4_2 = -radius + x1
+
+        y4_1 = y4_2 = y1
+        y3_1 = y3_2 = y2
+    
+    else:
+        seg_len = ((x2 - x1)**2 + (y2 - y1)**2) ** (1/2)
+
+        # Считаем для x4, y4
+        suby1 = (radius * (x2 - x1)) / seg_len
+        k1 = (suby1 * (y2 - y1)) / (x2 - x1)
+
+        y4_1 = -suby1 + y1
+        y4_2 = suby1 + y1
+        x4_1 = k1 + x1
+        x4_2 = -k1 + x1
+
+
+        # Считаем для x3, y3
+        suby2 = (radius * (x2 - x1)) / seg_len
+        k2 = (suby2 * (y2 - y1)) / (x2 - x1)
+
+        y3_1 = -suby1 + y2
+        y3_2 = suby1 + y2
+        x3_1 = k2 + x2
+        x3_2 = -k2 + x2
+
+
+    return x4_1, x4_2, y4_1, y4_2, x3_1, x3_2, y3_1, y3_2
+
+
 def pseudo_minkowski_sum(polygon, radius):
     new_polygon = []
 
     for p in polygon:
-        sub_points = [Point(p.x - radius, p.y + radius), Point(p.x - radius, p.y - radius),
-                        Point(p.x + radius, p.y + radius), Point(p.x + radius, p.y - radius)]
+        sub_points = [Point(p.x - radius, p.y - radius), Point(p.x + radius, p.y - radius),
+                        Point(p.x + radius, p.y + radius), Point(p.x - radius, p.y + radius), Point(p.x - radius, p.y - radius)]
 
         for sub in sub_points:
-            if not check_point_in_polygon([polygon], Point(sub.x, sub.y - 5)) \
-                    and not check_point_in_polygon([polygon], Point(sub.x, sub.y + 5)) \
-                    and not check_point_in_polygon([polygon], Point(sub.x - 5, sub.y)) \
-                    and not check_point_in_polygon([polygon], Point(sub.x + 5, sub.y)):
-                new_polygon.append(sub)
+            new_polygon.append(sub)
 
     return new_polygon
 
