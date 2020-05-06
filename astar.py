fb56e1f63bc0afff5d91b32800dec67131bfbf8b
Samvel <41449047+sttie@users.noreply.github.com>
2020-05-06 04:00:11 +0300

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,34 +1,9 @@
-# Очередь с приоритетом
-class PriorityQueue:
-    def __init__(self):
-        self.queue = []
-        self.size = 0
+from heapq import heappush as push, heappop as pop
 
-    def push(self, value, weight):
-        idx = 0
-        while idx < self.size and self.queue[idx][1] <= weight:
-            idx += 1
-
-        if idx == self.size:
-            self.queue.append((value, weight))
-        else:
-            self.queue.insert(idx, (value, weight))
-
-        self.size += 1
-
-    def pop_front(self):
-        self.size -= 1
-        return self.queue.pop(0)
-    
-    def is_empty(self):
-        return self.size == 0
-
-
-"""
-Эвристическая оценка h(v) - евклидово расстояние от точки v до конечной
-"""
+# Эвристическая оценка h(v) - евклидово расстояние от точки v до конечной
 def heuristics(v, end):
     return ((v.x - end.x)**2 + (v.y - end.y)**2)**(1/2)
+
 
 """
 В процессе работы рассчитывается функция f(v) = g(v) + h(v), где
@@ -37,29 +12,33 @@ h(v) - эвристическое приближение стоимости пу
 Открытые алгоритмом вершины хранятся в очереди с приоритетом по значению f(v)
 """
 def astar_algo(graph, start, end, all_points):
-    to_visit = PriorityQueue()
-    to_visit.push(start, 0)
+    astar_visited = []
+    to_visit = []
+    push(to_visit, (0, start))
     ancestors = [0] * graph.vertex_amount
     lengths = [0] * graph.vertex_amount
     lengths[start] = 0
 
-    while not to_visit.is_empty():
-        current_vert = to_visit.pop_front()[0]
+    while len(to_visit) > 0:
+        current_vert = pop(to_visit)[1]
+
         if current_vert == end:
             break
 
+        astar_visited.append(current_vert)
+
         for v in range(graph.vertex_amount):
-            if graph.matrix[current_vert][v] == 0:
+            if not graph.get_weight(current_vert, v):
                 continue
 
             cost = lengths[current_vert] + graph.get_weight(current_vert, v)
             if not lengths[v] or cost < lengths[v]:
                 lengths[v] = cost
                 prior = cost + heuristics(all_points[v], all_points[end])
-                to_visit.push(v, prior)
+                push(to_visit, (prior, v))
                 ancestors[v] = current_vert
 
-    return find_path(start, end, ancestors, all_points)
+    return find_path(start, end, ancestors, all_points), astar_visited
 
 
 def find_path(start, end, ancestors, all_points):
@@ -72,5 +51,3 @@ def find_path(start, end, ancestors, all_points):
     path.append(all_points[start])
 
     return path[::-1]
-
-
