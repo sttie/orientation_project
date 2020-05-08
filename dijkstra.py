4689f8c809f8100a3ea9078e32a0b63500b330d7
Samvel <41449047+sttie@users.noreply.github.com>
2020-05-08 17:09:17 +0300

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -1,7 +1,39 @@
 from heapq import heappush as push, heappop as pop
 
-# Ширина окна - 1200, высота - 900 => максимальная (округленная) длина ребра - 1500
-MAXLEN = 1500
+
+def dijkstra_algo(graph, start, end, all_points):
+    dijkstra_visited = []
+    to_visit = []
+    lengths = [0] * graph.vertex_amount
+    ancestors = [0] * graph.vertex_amount
+    visited = [0] * graph.vertex_amount
+    
+    push(to_visit, (0, start))
+    lengths[start] = 0
+
+    while len(to_visit) > 0:
+        current_vert = pop(to_visit)[1]
+
+        if visited[current_vert]:
+            continue
+
+        if current_vert == end:
+            break
+
+        dijkstra_visited.append(current_vert)
+
+        for neighbor in graph.matrix[current_vert]:
+            neighbor_num, weight = neighbor[0], neighbor[1]
+            cost = lengths[current_vert] + weight
+            if not lengths[neighbor_num] or cost < lengths[neighbor_num]:
+                lengths[neighbor_num] = cost
+                push(to_visit, (cost, neighbor_num))
+                ancestors[neighbor_num] = current_vert
+
+        visited[current_vert] = 1
+
+    return find_path(start, end, ancestors, all_points), dijkstra_visited
+
 
 def find_path(start, end, ancestors, all_points):
     path = []
@@ -13,33 +45,3 @@ def find_path(start, end, ancestors, all_points):
     path.append(all_points[start])
 
     return path[::-1]
-
-
-def dijkstra_algo(graph, start, end, all_points):
-    dijkstra_visited = []
-    to_visit = []
-    lengths = [0] * graph.vertex_amount
-    ancestors = [0] * graph.vertex_amount
-    
-    push(to_visit, (0, start))
-    lengths[start] = 0
-
-    while len(to_visit) > 0:
-        current_vert = pop(to_visit)[1]
-
-        if current_vert == end:
-            break
-
-        dijkstra_visited.append(current_vert)
-
-        for neighbor in range(graph.vertex_amount):
-            if not graph.get_weight(current_vert, neighbor):
-                continue
-
-            cost = lengths[current_vert] + graph.get_weight(current_vert, neighbor)
-            if not lengths[neighbor] or cost < lengths[neighbor]:
-                lengths[neighbor] = cost
-                push(to_visit, (cost, neighbor))
-                ancestors[neighbor] = current_vert
-
-    return find_path(start, end, ancestors, all_points), dijkstra_visited
